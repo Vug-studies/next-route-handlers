fa535253226c0a6071d51255be26b17d5793f0f9
namevug <ciavug@gmail.com>
2023-05-24 21:59:32 -0300

    feat: Route Resolution

---
+++ src/app/product/api/product/route.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server"
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url)
+  const id = searchParams.get('id')
+
+  // const users = await fetch('https://jsonplaceholder.typicode.com/users')
+  // const userJson = await users.json()
+
+  return NextResponse.json({ id })
+}
